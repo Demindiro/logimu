219ed61dd75d32a4c700a98556ed8de54862c5a6
David Hoppenbrouwers <david@salt-inc.org>
2021-12-13 12:49:18 +0100

    Use ScrollArea in "Open file" listing

--- src/gui/file.rs
+++ src/gui/file.rs
@@ -1,3 +1,4 @@
+use eframe::egui;
 use std::fs;
 
 pub struct OpenDialog {}
@@ -7,15 +8,17 @@ impl super::Dialog for OpenDialog {
 		"Open file"
 	}
 
-	fn show(&mut self, ui: &mut eframe::egui::Ui) -> bool {
-		let dir = fs::read_dir(".").unwrap();
-		for e in dir {
-			let e = e.unwrap();
-			if ui.button(e.file_name().to_string_lossy()).clicked() {
-				println!("Pressed {}", e.file_name().to_string_lossy());
-				return true;
+	fn show(&mut self, ui: &mut egui::Ui) -> bool {
+		egui::ScrollArea::new([false, true]).show(ui, |ui| {
+			let dir = fs::read_dir(".").unwrap();
+			for e in dir {
+				let e = e.unwrap();
+				if ui.button(e.file_name().to_string_lossy()).clicked() {
+					println!("Pressed {}", e.file_name().to_string_lossy());
+					return true;
+				}
 			}
-		}
-		false
+			false
+		})
 	}
 }
